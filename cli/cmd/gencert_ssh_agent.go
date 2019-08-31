6aa2a9f9f10edf0e2cca63908b1418eaae2d896e
Alexey Vasiliev <leopard.not.a@gmail.com>
2019-09-01 02:47:53 +0300

    support ssh agent

--- cli/cmd/gencert_ssh_agent.go
+++ cli/cmd/gencert_ssh_agent.go
@@ -1,22 +1,77 @@
 package cmd
 
 import (
+	"crypto/dsa"
+	"crypto/ecdsa"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"os"
 	"time"
 
+	"github.com/ScaleFT/sshkeys"
 	"github.com/le0pard/certonid/utils"
 	homedir "github.com/mitchellh/go-homedir"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ed25519"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/agent"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+func genGetPrivateKeyPassphrase(data []byte) ([]byte, error) {
+	var (
+		passPhrase    []byte
+		passPhraseErr error
+	)
+
+	block, _ := pem.Decode(data)
+
+	_, setFlagForPassphrase := utils.GetENV("PRIVATE_KEY_PASSPHRASE")
+
+	if (block != nil && x509.IsEncryptedPEMBlock(block)) || setFlagForPassphrase {
+		fmt.Print("SSH Key Passphrase [none]: ")
+		passPhrase, passPhraseErr = terminal.ReadPassword(int(os.Stdin.Fd()))
+		fmt.Print("\n")
+		if passPhraseErr != nil {
+			log.WithFields(log.Fields{
+				"error": passPhraseErr,
+			}).Error("Not provided passphrase for provate key")
+			return nil, passPhraseErr
+		}
+	}
+
+	return passPhrase, nil
+}
+
+func genCastInterfaceToPrimaryKeyInterface(key interface{}) interface{} {
+	rsaKey, ok := key.(rsa.PrivateKey)
+	if ok {
+		return &rsaKey
+	}
+	dsaKey, ok := key.(dsa.PrivateKey)
+	if ok {
+		return &dsaKey
+	}
+	ecdsaKey, ok := key.(ecdsa.PrivateKey)
+	if ok {
+		return &ecdsaKey
+	}
+	ed25519Key, ok := key.(ed25519.PrivateKey)
+	if ok {
+		return &ed25519Key
+	}
+	return nil
+}
+
 func genAddCertToAgent(cert *ssh.Certificate) error {
 	var (
-		privateKey    ssh.Signer
+		privateKey    interface{}
+		privateKeyRaw interface{}
 		privateKeyErr error
 	)
 	expandedPrivateKey, err := homedir.Expand(genAddToSSHAgent)
@@ -37,22 +92,16 @@ func genAddCertToAgent(cert *ssh.Certificate) error {
 		return err
 	}
 
-	_, ok := utils.GetENV("PRIVATE_KEY_PASSPHRASE")
-	if ok {
-		fmt.Print("SSH Key Passphrase [none]: ")
-		passPhrase, err := terminal.ReadPassword(int(os.Stdin.Fd()))
-		fmt.Print("\n")
-		if err != nil {
-			log.WithFields(log.Fields{
-				"error": err,
-			}).Error("Not provided passphrase for provate key")
-			return err
-		}
-
-		privateKey, privateKeyErr = ssh.ParsePrivateKeyWithPassphrase(privatKeyBytes, []byte(passPhrase))
-	} else {
-		privateKey, privateKeyErr = ssh.ParsePrivateKey(privatKeyBytes)
+	passPhrase, err := genGetPrivateKeyPassphrase(privatKeyBytes)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":    err,
+			"filename": expandedPrivateKey,
+		}).Error("Could not get passphrase for private key")
+		return err
 	}
+
+	privateKeyRaw, privateKeyErr = sshkeys.ParseEncryptedRawPrivateKey(privatKeyBytes, passPhrase)
 
 	if privateKeyErr != nil {
 		log.WithFields(log.Fields{
@@ -62,10 +111,44 @@ func genAddCertToAgent(cert *ssh.Certificate) error {
 		return err
 	}
 
-	agentKeyring := agent.NewKeyring()
+	privateKey = genCastInterfaceToPrimaryKeyInterface(privateKeyRaw)
+	if privateKey == nil {
+		log.WithFields(log.Fields{
+			"error":    privateKeyErr,
+			"filename": expandedPrivateKey,
+		}).Error("Unknown private key format")
+		return err
+	}
+
+	authSock := os.Getenv("SSH_AUTH_SOCK")
+	if authSock == "" {
+		log.WithFields(log.Fields{
+			"error":    privateKeyErr,
+			"filename": expandedPrivateKey,
+		}).Error("SSH_AUTH_SOCK environment variable empty")
+		return errors.New("SSH_AUTH_SOCK environment variable empty")
+	}
+	agentSock, err := net.Dial("unix", authSock)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":    privateKeyErr,
+			"filename": expandedPrivateKey,
+		}).Error("ssh-agent is not working on SSH_AUTH_SOCK socket")
+		return err
+	}
+	defer agentSock.Close()
+
+	agentKeyring := agent.NewClient(agentSock)
 
 	t := time.Unix(int64(cert.ValidBefore), 0)
 	lifetime := t.Sub(time.Now()).Seconds()
+
+	if privateKey == nil {
+		log.WithFields(log.Fields{
+			"error":    privateKeyErr,
+			"filename": expandedPrivateKey,
+		}).Error("Unknow private key format")
+	}
 
 	pubcert := agent.AddedKey{
 		PrivateKey:   privateKey,
@@ -84,7 +167,6 @@ func genAddCertToAgent(cert *ssh.Certificate) error {
 	}
 
 	log.WithFields(log.Fields{
-		"error":       err,
 		"filename":    expandedPrivateKey,
 		"valid until": time.Unix(int64(cert.ValidBefore), 0).UTC(),
 	}).Info("Cetificate successfully added to ssh-agent")
--- cli/go.mod
+++ cli/go.mod
@@ -3,7 +3,9 @@ module github.com/le0pard/certonid/cli
 go 1.12
 
 require (
+	github.com/ScaleFT/sshkeys v0.0.0-20181112160850-82451a803681
 	github.com/aws/aws-sdk-go v1.23.10 // indirect
+	github.com/dchest/bcrypt_pbkdf v0.0.0-20150205184540-83f37f9c154a // indirect
 	github.com/le0pard/certonid v0.0.0-20190828173011-a56b4d69c116
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/sirupsen/logrus v1.4.2
