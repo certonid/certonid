6a4951b50597252f62273444bc6c861a56f62cb5
Alexey Vasiliev <leopard.not.a@gmail.com>
2020-12-17 02:02:18 +0200

    right now only linux arm available

--- scripts/build/binary.sh
+++ scripts/build/binary.sh
@@ -13,7 +13,7 @@ echo "Building statically linked certonid"
 export CGO_ENABLED=0
 # cli
 gox -os="linux" -arch="amd64 arm64" -output="build/certonid.{{.OS}}.{{.Arch}}" -ldflags "${LDFLAGS}" -verbose ./cli
-gox -os="darwin" -arch="amd64 arm64" -output="build/certonid.{{.OS}}.{{.Arch}}" -ldflags "${LDFLAGS}" -verbose ./cli
+gox -os="darwin" -arch="amd64" -output="build/certonid.{{.OS}}.{{.Arch}}" -ldflags "${LDFLAGS}" -verbose ./cli
 gox -os="windows" -arch="amd64" -output="build/certonid.{{.OS}}.{{.Arch}}" -ldflags "${LDFLAGS}" -verbose ./cli
 # serverless
 gox -os="linux" -arch="amd64" -output="build/serverless.{{.OS}}.{{.Arch}}" -ldflags "${LDFLAGS}" -verbose ./serverless
