7340aba39b69b424254a07751b9262d1406a0d2e
Alexey Vasiliev <leopard.not.a@gmail.com>
2021-02-28 23:55:14 +0200

    build mac os m1 binary

--- scripts/build/binary.sh
+++ scripts/build/binary.sh
@@ -12,6 +12,6 @@ source ./scripts/build/.variables
 echo "Building statically linked certonid"
 export CGO_ENABLED=0
 # cli
-gox -osarch="linux/amd64 linux/arm64 darwin/amd64 windows/amd64" -output="build/certonid.{{.OS}}.{{.Arch}}" -ldflags "${LDFLAGS}" -verbose ./cli
+gox -osarch="linux/amd64 linux/arm64 darwin/amd64 darwin/arm64 windows/amd64" -output="build/certonid.{{.OS}}.{{.Arch}}" -ldflags "${LDFLAGS}" -verbose ./cli
 # serverless
 gox -osarch="linux/amd64" -output="build/serverless.{{.OS}}.{{.Arch}}" -ldflags "${LDFLAGS}" -verbose ./serverless
